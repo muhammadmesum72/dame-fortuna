5c6d7f0a615415c12ab5c27b65e6f544a958cf39
muhammadmesum72 <me.m.mesum@gmail.com>
2023-08-14 07:35:54 +0500

    Responsiveness Fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,11 +9,11 @@ import Gitbook from "../assets/gitbook.png"
 
 const Footer = () => {
   return (
-    <div id="footer" className="border-t border-white py-6  mt-36">
+    <div id="footer" className="border-t border-white py-6  mt-36 text-xs md:text-auto" >
       <div className="flex flex-col md:flex-row gap-3 md:gap-0 px-3 md:px-0 items-center container mx-auto">
-        <div className="basis-1/2 text-center md:text-left">
+        <div className="basis-1/2 text-center md:text-left ">
           <h2 className="text-xl font-bold  pb-3">LOGO</h2>
-          <p>
+          <p className="">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. A ipsa
             quasi necessitatibus officiis sed laboriosam soluta reprehenderit
             sit excepturi minus sapiente accusamus deserunt dolor autem, id nam
